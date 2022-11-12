880080905add8c24b3a070c806bb81daa8b0355d
Peter <gault.peter@gmail.com>
2022-11-12 16:49:36 +0000

    Updated manifest to fix domain to wildcard

--- firefox/redirect.js
+++ firefox/redirect.js
@@ -1,17 +1,15 @@
-const scriptElements = document.getElementsByTagName('script');
+const scriptElements = document.getElementsByTagName("script");
 
 let redirectRegex = /window.location.href =/;
-let urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+let urlRegex =
+	/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 
 for (let script of scriptElements) {
+	let isRedirectPresent = redirectRegex.test(script.innerHTML);
 
-    let isRedirectPresent = redirectRegex.test(script.innerHTML);
-
-    if (isRedirectPresent) {
-
-        const urls = script.innerHTML.match(urlRegex);
-        const redirectUrl = urls[0];
-        window.location.href = redirectUrl;
-
-    }
+	if (isRedirectPresent) {
+		const urls = script.innerHTML.match(urlRegex);
+		const redirectUrl = urls[0];
+		window.location.href = redirectUrl;
+	}
 }
